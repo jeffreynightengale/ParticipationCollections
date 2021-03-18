1ab0d080632d0e02f9b58d128cd59d68500d94e9
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-03-18 18:31:02 -0500

    started

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Please input your grades with a space between each one");
+            string grades = Console.ReadLine();
+
+            List<double> studentGrades = new List<double>();
+            string[] pieces = 
+        }
+    }
+}
